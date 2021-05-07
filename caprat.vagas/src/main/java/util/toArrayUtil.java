6afacd0d054d092c12e987f8ddd2e12128852900
Lucas Vinicius Sousa Silva <lucas2606.vinicius@gmail.com>
2021-05-07 03:58:13 -0300

    Modularizando a função de retornar um array com os dados do objeto

---
+++ caprat.vagas/src/main/java/util/toArrayUtil.java
@@ -0,0 +1,66 @@
+package util;
+
+import models.EmpresaVagas;
+import models.UsuarioCurriculo;
+import models.UsuarioLogin;
+import models.UsuarioVagas;
+
+public class toArrayUtil {
+	
+	public String[] toArray(UsuarioCurriculo curriculo) {
+		return (
+				new String[] {
+						""+curriculo.getIdCurriculo(),
+						""+curriculo.getIdUsuario(),
+						""+curriculo.getNomeUsuario(),
+						""+curriculo.getNascimentoUsuario(),
+						""+curriculo.getEnderecoUsuario(),
+						""+curriculo.getCidadeUsuario(),
+						""+curriculo.getEstadoUsuario(),
+						""+curriculo.getEstadoCivilUsuario(),
+						""+curriculo.getEscolaridadeUsuario(),
+						""+curriculo.getDescricaoUsuario(),
+						""+curriculo.getContatoUsuario(),
+						""+curriculo.getCursosUsuario(),
+						""+curriculo.getExpTrabUsuario(),
+						""+curriculo.getFaixaSalarialUsuario()
+				}
+		); 
+	}
+	
+	public String[] toArray(EmpresaVagas vaga) {
+		return(
+				new String [] {
+						""+vaga.getIdVaga(),
+						""+vaga.getIdEmpresa(),
+						""+vaga.getTituloVaga(),
+						""+vaga.getRequisitosVaga(),
+						""+vaga.getDescricaoVaga(),
+						""+vaga.getLocalVaga(),
+						""+vaga.getSalarioVaga(),
+						""+vaga.getNivelExpVaga()
+				}
+			);
+	}
+	
+	public String[] toArray(UsuarioLogin dadosLoginUsuario) {
+		return (
+				new String[] {
+					""+dadosLoginUsuario.getIdUsuario(),
+					""+dadosLoginUsuario.getEmailUsuario(),
+					""+dadosLoginUsuario.getSenhaUsuario(),
+					""+dadosLoginUsuario.getTipoUsuario()
+				}
+ 			);
+	}
+	
+	
+	public String[] toArray(UsuarioVagas vagasDoUsuario) {
+		return (
+				new String[] {
+					""+vagasDoUsuario.getIdUsuario(),
+					""+vagasDoUsuario.getIdVaga()
+				}
+ 			);
+	}
+}
